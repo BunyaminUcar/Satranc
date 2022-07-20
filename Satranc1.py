5ebfda614e100585d649b0aa192523116c754eba
BunyaminUcar <190542015@firat.edu.tr>
2022-07-20 14:47:49 +0300

    Update Satranc1.py
    İstisnai engele durumu kontrolü yapıldı

--- Satranc1.py
+++ Satranc1.py
@@ -1,3 +1,5 @@
+engel=[(5,1),(4,6),(2,6),(1,4),(1,8),(8,1),(8,8)]
+
 deger=63
 x_ekseni=8
 y_ekseni=8
@@ -17,10 +19,9 @@ print(x_koordinat)
 print(y_koordinat)
 
 
-
 #At için muhtemel hareket koordinatları
-x=1
-y=8
+x=5
+y=4
 At=[]
 At.append((x + 2, y - 1))
 At.append((x + 2, y + 1))
@@ -31,9 +32,12 @@ At.append((x + 1, y - 2))
 At.append((x - 1, y + 2))
 At.append((x - 1, y - 2))
 
-y=[i for i in At if all([ 0<i[0]<9 , 0<i[1]<9 ])]
+y=[i for i in At if all([ 0<i[0]<9 , 0<i[1]<9 ]) and i not in engel]
+
+
 
 print("At :",y)
+
 
 #Fil için muhtemel hareket koordinatları
 
@@ -48,7 +52,7 @@ for n in range(1,8):
     fil.append((x+n, y-n))
 
 
-y1=[i for i in fil if all([ 0<i[0]<9 , 0<i[1]<9 ])]
+y1=[i for i in fil if all([ 0<i[0]<9 , 0<i[1]<9 ]) and i not in engel]
 
 print("Fil :",y1)
 #Şah için muhtemel hareket koordinatları
@@ -65,7 +69,7 @@ sah.append((x+1, y-1))
 sah.append((x, y+1))
 sah.append((x, y-1))
 
-y2=[i for i in sah if all([ 0<i[0]<9 , 0<i[1]<9 ])]
+y2=[i for i in sah if all([ 0<i[0]<9 , 0<i[1]<9 ]) and i not in engel]
 
 print("Sah :",y2)
 #Kale için muhtemel hareket koordinatları
@@ -84,7 +88,7 @@ for n in range(1,8):
     kale.append((x+n, y))
     kale.append((x-n, y))
 
-y3=[i for i in kale if all([ 0<i[0]<9 , 0<i[1]<9 ])]
+y3=[i for i in kale if all([ 0<i[0]<9 , 0<i[1]<9 ]) and i not in engel]
 print("Kale  :",y3)
 
 
@@ -103,6 +107,6 @@ for n in range(1,8):
     vezir.append((x+n, y))
     vezir.append((x-n, y))
 
-y4=[i for i in vezir if all([ 0<i[0]<9 , 0<i[1]<9 ])]
+y4=[i for i in vezir if all([ 0<i[0]<9 , 0<i[1]<9 ]) and i not in engel]
 
 print("Vezir :",y4)
